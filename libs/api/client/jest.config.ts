899c279b9f6b3e87ea724e1f3368527f16c35839
marcus-sa <marcus-sa@outlook.com>
2023-12-21 09:38:22 +0100

    save

--- libs/api/client/jest.config.ts
+++ libs/api/client/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'api-client',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   testEnvironment: 'node',
-  coverageDirectory: '../../coverage/libs/api/client',
+  coverageDirectory: '../../../coverage/libs/api/client',
 };
--- libs/api/server/jest.config.ts
+++ libs/api/server/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'api-server',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   testEnvironment: 'node',
-  coverageDirectory: '../../coverage/libs/api/server',
+  coverageDirectory: '../../../coverage/libs/api/server',
 };
