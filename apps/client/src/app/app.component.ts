65fdddd53c23f19f36d225693f48a929e81871c0
marcus-sa <marcus-sa@outlook.com>
2023-12-22 08:13:25 +0100

    feat(client): setup shroom

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Application } from 'pixi.js';
+import { Shroom } from '@jankuss/shroom';
 
 @Component({
   selector: 'apex-root',
@@ -7,4 +9,28 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  @ViewChild('canvas') canvas: ElementRef<HTMLDivElement>;
+
+  #shroom: Shroom | undefined;
+  get shroom(): Shroom {
+    if (!this.#shroom) {
+      throw new Error('No Shroom instance');
+    }
+    return this.#shroom
+  }
+
+  #pixi: Application | undefined;
+  get pixi(): Application {
+    if (!this.#pixi) {
+      throw new Error('No Pixi application instance');
+    }
+    return this.#pixi
+  }
+
+  async ngOnInit() {
+    this.#pixi = new Application();
+    await this.#pixi.init();
+    this.canvas.nativeElement.appendChild(this.#pixi.canvas);
+  }
+}
